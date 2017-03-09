c4bf04e2c28a6a794f530057fde88a2a83fc924f
Karl Li <killtw@gmail.com>
2017-03-09 12:41:13 +0800

    add default layout

---
+++ app/src/renderer/components/Layout/Default.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-drawer mdl-layout--fixed-header">
+    <top></top>
+    <sidebar></sidebar>
+    <main class="mdl-layout__content">
+      <div class="page-content">
+        ttest
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+  import componentHandler from 'material-design-lite';
+  import Sidebar from './Sidebar';
+  import Top from './Header';
+  import Main from '../Main';
+
+  export default {
+    ready: () => {
+      componentHandler.upgradeDom();
+    },
+    components: {
+      Sidebar,
+      Top,
+    },
+    routes: [
+      {
+        path: '/',
+        component: Main,
+      },
+    ],
+  };
+</script>
---
+++ app/src/renderer/components/Layout/Sidebar.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="mdl-layout__drawer">
+    <span class="mdl-layout-title">VCD</span>
+    <nav class="mdl-navigation menus">
+      <a class="mdl-navigation__link" href="">Queues</a>
+      <a class="mdl-navigation__link" href="">Library</a>
+      <a class="mdl-navigation__link" href="">History</a>
+    </nav>
+  </div>
+</template>
