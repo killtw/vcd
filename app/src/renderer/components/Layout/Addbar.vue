b82b59282a662ff742f184319f53089ba10fac33
Karl Li <killtw@gmail.com>
2017-03-17 11:28:26 +0800

    add url and parse

--- app/src/renderer/components/Layout/Addbar.vue
+++ app/src/renderer/components/Layout/Addbar.vue
@@ -2,20 +2,37 @@
     <header class="mdl-layout__header mdl-color--grey-100 mdl-color-text--grey-600">
         <div class="mdl-layout__header-row">
             <div class="mdl-layout-spacer"></div>
-            <div class="mdl-textfield mdl-js-textfield mdl-textfield--expandable mdl-textfield--floating-label mdl-textfield--align-right">
+            <div class="mdl-textfield mdl-js-textfield mdl-textfield--expandable mdl-textfield--align-right">
                 <label class="mdl-button mdl-js-button mdl-button--icon" for="add_button">
                     <i class="material-icons">add_circle_outline</i>
                 </label>
                 <div class="mdl-textfield__expandable-holder widefield">
-                    <input class="mdl-textfield__input" type="text" name="url" id="add_button">
+                    <input class="mdl-textfield__input" type="text" id="add_button" @keyup.enter="addUrl">
                 </div>
             </div>
-            <button class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon">
-                <i class="material-icons">file_download</i>
-            </button>
+
+            <mdl-button icon="file_download" class="mdl-js-ripple-effect"></mdl-button>
         </div>
     </header>
 </template>
+
+<script>
+  import { mapActions } from 'vuex';
+
+  export default {
+    methods: {
+      addUrl(e) {
+        const url = e.target.value;
+        e.target.value = null;
+        this.$el.querySelector('.mdl-textfield').MaterialTextfield.change();
+        if (url.trim()) {
+          this.add(url);
+        }
+      },
+      ...mapActions('sites', ['add']),
+    },
+  };
+</script>
 
 <style scoped>
     .widefield {
