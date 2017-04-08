1fa6a8456e668c9cc3307cc035a9312c46e63f05
Karl Li <killtw@gmail.com>
2017-04-08 15:27:59 +0800

    add datetime filter

--- app/src/renderer/filters.js
+++ app/src/renderer/filters.js
@@ -1,6 +1,9 @@
+import moment from 'moment';
+
 export function install(Vue) {
-  Vue.filter('percent', (value, n) => `${Number(value).toFixed(n || 4) * 100} %`);
-  Vue.filter('decimal', (value, n) => Number(value).toFixed(n || 2));
-  Vue.filter('uppercase', (value) => value.toUpperCase());
-  Vue.filter('ucfirst', (value) => `${value.charAt(0).toUpperCase()}${value.slice(1)}`);
+  Vue.filter('percent', (value, n = 4) => `${Number(value).toFixed(n) * 100} %`);
+  Vue.filter('decimal', (value, n = 2) => Number(value).toFixed(n));
+  Vue.filter('uppercase', value => value.toUpperCase());
+  Vue.filter('ucfirst', value => `${value.charAt(0).toUpperCase()}${value.slice(1)}`);
+  Vue.filter('datetime', (value, format = 'Y-MM-DD HH:mm:ss') => moment(value).format(format));
 }
